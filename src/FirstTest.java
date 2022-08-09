ea99e69c60bf3f2a397a0fbc20e34db2c2aa05a8
Ksenia Druzhinskaya <Ksenia.Druzhinskaya@firstlinesoftware.com>
2022-08-09 19:20:42 +0300

    Initial commit

---
+++ src/FirstTest.java
@@ -0,0 +1,38 @@
+import io.appium.java_client.AppiumDriver;
+import io.appium.java_client.android.AndroidDriver;
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+import org.openqa.selenium.remote.DesiredCapabilities;
+
+import java.net.URL;
+
+public class FirstTest
+{
+    private AppiumDriver driver;
+
+    @Before
+    public void setUp() throws Exception{
+        DesiredCapabilities capabilities = new DesiredCapabilities();
+
+        capabilities.setCapability("platformName", "Android");
+        capabilities.setCapability("deviceName", "AndroidDevice");
+        capabilities.setCapability("platformName", "Android");
+        capabilities.setCapability("platformVersion", "8.0");
+        capabilities.setCapability("appPackage", "org.wikipedia");
+        capabilities.setCapability("appActivity", ".main.MainActivity");
+        capabilities.setCapability("app", "D:\\JavaAppiumAutomation\\apks\\org.wikipedia_50417_apps.evozi.com.apk");
+
+        driver = new AndroidDriver(new URL("http://127.0.0.1:4723/wd/hub"), capabilities);
+    }
+
+    @After
+    public void teadDown(){
+        driver.quit();
+    }
+
+    @Test
+    public void firstTest(){
+        System.out.println("First test run");
+    }
+}
